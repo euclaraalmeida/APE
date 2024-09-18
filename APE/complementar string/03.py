f7de6045c806c19fe7050d632f08776fe3d0fed2
Maria Clara Almeida <164226794+euclaraalmeida@users.noreply.github.com>
2024-09-18 18:14:34 -0300

    Update 03.py

--- APE/complementar string/03.py
+++ APE/complementar string/03.py
@@ -1,5 +1,5 @@
 nome=input().upper()
 divisao=nome.split( )
-for i in range(nome):
-    imp=i[0]
-print(f'{i[-1]},{[0]}.{i[1][0]}.')
+print(f'{divisao[-1]}',end=',')
+for j in range(len(divisao)-1):
+    print(f'{divisao[j][0]}',end='.')
