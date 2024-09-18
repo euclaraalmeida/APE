1c8864bcb76b945d70714f5731dbe642306fd3ad
Maria Clara Almeida <164226794+euclaraalmeida@users.noreply.github.com>
2024-09-18 18:15:03 -0300

    Update 05.py

--- APE/complementar string/05.py
+++ APE/complementar string/05.py
@@ -1,5 +1,9 @@
-frase=input('')
-frases=' '
-for i in range(frases):
-    frases*=i
-print(frases)
+frase=(input(''))
+resul=''
+for i in range(len(frase)):
+        resul=frase[i]*(i+1)
+        print(resul) 
+for i in range(len(frase)-2,-1,-1):
+        resul=frase[i]*(i+1)
+        print(resul) 
+
